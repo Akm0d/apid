98358a84f79afff268fd854086068677e2c4caf3
Tyler Johnson <tjohnson@saltstack.com>
2019-09-24 14:57:27 -0600

    Prototyped the CLI config

--- apid/apid/init.py
+++ apid/apid/init.py
@@ -1,3 +1,21 @@
 def __init__(hub):
     print('apid works!')
-    #hub.pop.sub.add(dyne_name='idem')
+    hub.pop.conf.integrate('apid', cli='apid', roots=True)
+    hub.apid.init.load_subs()
+    hub.apid.init.validate_config()
+
+
+def load_subs(hub):
+    '''
+    Load up the needed subs
+    '''
+    # TODO? Does salt the aws module import apid, or does apid load the aws module
+    # TODO? Based on the profile that's configured
+    # hub.pop.sub.add('heis')
+    # hub.pop.sub.add(dyne_name='idem')
+    # hub.pop.sub.add(dyne_name='rend')
+
+
+def validate_config(hub):
+    assert hub.OPT['apid']['profile'], 'No profiles configured'
+    assert hub.OPT['apid']['provider'], 'No providers configured'
--- apid/conf.py
+++ apid/conf.py
@@ -1,4 +1,48 @@
-CLI_CONFIG = {}
+CLI_CONFIG = {
+    'user': {
+        'default': 'root',
+        'type': str,
+        'help': 'The salt master user',
+    },
+    'profile': {
+        'default': None,
+        'action': 'append',
+        'help': 'The absolute path to a cloud profile config',
+    },
+    'provider': {
+        'default': None,
+        'action': 'append',
+        'help': 'The absolute path to a cloud provider config',
+    },
+    'maps': {
+        'default': None,
+        'help': 'The map dict from pillars'
+    },
+    'list_images': {
+        'action': 'store_true',
+    },
+    'list_locations': {
+        'action': 'store_true',
+    },
+    'list_profiles': {
+        'action': 'store_true',
+    },
+    'list_providers': {
+        'action': 'store_true',
+    },
+    'list_sizes': {
+        'action': 'store_true',
+    },
+    'action': {
+    },
+    'destroy': {
+        'action': 'store_true',
+    },
+    'function': {
+        'action': 'store_true',
+    },
+}
+
 CONFIG = {}
 GLOBAL = {}
 SUBS = {}
